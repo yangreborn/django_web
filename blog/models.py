fda66a721c58b46de2a894a933680fc8029933b0
yang <yjw728834@163.com>
2025-04-01 15:11:29 +0800

    添加缺失依赖timezone

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, User
 from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
 
 class CustomUser(AbstractUser):
     def __str__(self):
